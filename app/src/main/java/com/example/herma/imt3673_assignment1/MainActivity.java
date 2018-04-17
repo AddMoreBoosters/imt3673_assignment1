93c40450175380d3c580c5360af37f1dcdbc93ca
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2018-04-17 13:08:16 +0200

    Getting rid of excess options

--- app/src/main/java/com/example/herma/imt3673_assignment1/MainActivity.java
+++ app/src/main/java/com/example/herma/imt3673_assignment1/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void StartGame(View view)
+    {
+        
+    }
 }
