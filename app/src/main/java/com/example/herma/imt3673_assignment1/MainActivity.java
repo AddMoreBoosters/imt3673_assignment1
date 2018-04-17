578afd764622f26568c6d311928d5230a3bd6b31
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2018-04-17 14:16:51 +0200

    The play button now starts lab 3 in a new activity

--- app/src/main/java/com/example/herma/imt3673_assignment1/MainActivity.java
+++ app/src/main/java/com/example/herma/imt3673_assignment1/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 
 import com.google.android.gms.common.api.GoogleApi;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void StartGame(View view)
     {
-        
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
